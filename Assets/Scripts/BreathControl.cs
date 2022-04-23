d8861eda66e2967b0f30e303decf13cfc4f0c1ef
LauraMarsoner <laura.marsoner@hotmail.com>
2022-04-23 17:45:37 +0200

    Projectiles added, projectile movement

--- Assets/Scripts/BreathControl.cs
+++ Assets/Scripts/BreathControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,14 @@ public class BreathControl : MonoBehaviour
     public VisualEffect fireBreath;
     public VisualEffect iceBreath;
     public VisualEffect electroBreath;
+
+    public GameObject fireProjectile;
+    public GameObject iceProjectile;
+    public GameObject electroProjectile;
+
+    public GameObject fireStart;
+    public GameObject iceStart;
+    public GameObject electroStart;
     
     void Start()
     {
@@ -22,29 +31,67 @@ public class BreathControl : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             fireBreath.Play();
+            SpawnProjectile(fireProjectile,fireStart);
         }
-
         if (Input.GetMouseButtonUp(0))
         {
             fireBreath.Stop();
         }
+        
+        
         if (Input.GetMouseButtonDown(1))
         {
             iceBreath.Play();
+            SpawnProjectile(iceProjectile,iceStart);
         }
-
         if (Input.GetMouseButtonUp(1))
         {
             iceBreath.Stop();
         }
+        
+        
         if (Input.GetMouseButtonDown(2))
         {
             electroBreath.Play();
+            SpawnProjectile(electroProjectile,electroStart);
         }
-
         if (Input.GetMouseButtonUp(2))
         {
             electroBreath.Stop();
         }
     }
+
+    public void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SpawnProjectile(fireProjectile,fireStart);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            SpawnProjectile(iceProjectile,iceStart);
+        }
+        if (Input.GetMouseButton(2))
+        {
+            SpawnProjectile(electroProjectile,electroStart);
+        }
+    }
+    
+    
+    public void SpawnProjectile(GameObject projectile, GameObject startPoint)
+    {
+        GameObject proj;
+        if (startPoint != null)
+        {
+            proj = Instantiate(projectile, startPoint.transform.position,startPoint.transform.rotation);
+            proj.transform.localPosition = startPoint.transform.position;
+            proj.transform.localRotation = startPoint.transform.rotation;
+        }
+        else
+        {
+            Debug.Log("Startpoint of Projectile not set!");
+        }
+    }
+
+
 }
